c9efaa81c6d54e94a1475e3f51b87488aafdae86
Verena <verenaprinz@web.de>
2025-04-16 16:19:17 +0200

    Passt noch nicht

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,24 +4,15 @@ import { ImpressumComponent } from "./impressum/impressum.component";
 import { TermsOfUseComponent } from "./terms-of-use/terms-of-use.component";
 import { RepaymentCalculatorComponent } from "./repayment-calculator/repayment-calculator.component";
 import { HowToComponent } from "./how-to/how-to.component";
-import { NgModule } from "@angular/core";
 
-const routes: Routes = [
-    { path: '', component: RentBuyCalculatorComponent },
-    { path: 'rent-vs-buy', component: RentBuyCalculatorComponent },
-    { path: 'repayment', component: RepaymentCalculatorComponent },
-    { path: 'how-to', component: HowToComponent },
-    { path: 'impressum', component: ImpressumComponent },
-    { path: 'terms-of-use', component: TermsOfUseComponent },
-  ]
-
-@NgModule({
-    imports: [RouterModule.forRoot(routes, {
-      useHash: false,
-      scrollPositionRestoration: 'enabled',
-    })],
-    exports: [RouterModule],
-  })
+export const appRoutes: Routes = [
+    {path: '', component:RentBuyCalculatorComponent},
+    {path: 'rent-vs-buy', component:RentBuyCalculatorComponent},
+    {path: 'repayment', component:RepaymentCalculatorComponent},
+    {path: 'how-to', component:HowToComponent},
+    {path:'impressum', component:ImpressumComponent},
+    {path:'terms-of-use', component:TermsOfUseComponent},
+]
   
 export class AppRoutingModule {
 
