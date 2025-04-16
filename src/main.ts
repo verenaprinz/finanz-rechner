2e244e054993c7b157e5e34f56b002a0a7d1fe2d
Verena <verenaprinz@web.de>
2025-04-16 20:52:02 +0200

    Fix Angular routing for GitHub Pages using 404 fallback

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,21 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 
 
+
+// bootstrapApplication(AppComponent, {
+//   providers: [
+//     provideRouter(appRoutes),
+//   ],
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(appRoutes),
-  ],
+    provideRouter(appRoutes, withNavigationFallback({
+      indexHtmlPath: '/finanz-rechner/404.html'  // wichtig für GitHub Pages
+    }))
+  ]
 }).catch(err => console.error(err));
+function withNavigationFallback(arg0: {
+  indexHtmlPath: string; // wichtig für GitHub Pages
+}): import("@angular/router").RouterFeatures {
+  throw new Error('Function not implemented.');
+}
+
