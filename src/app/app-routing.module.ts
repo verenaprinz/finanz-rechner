6c92e398cdf2db8f2b2cf593bf2973e08a01a871
Verena <verenaprinz@web.de>
2025-04-16 15:57:29 +0200

    Fix: routing config and build output location

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,28 @@
-import { Routes } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 import { RentBuyCalculatorComponent } from "./rent-buy-calculator/rent-buy-calculator.component";
 import { ImpressumComponent } from "./impressum/impressum.component";
 import { TermsOfUseComponent } from "./terms-of-use/terms-of-use.component";
 import { RepaymentCalculatorComponent } from "./repayment-calculator/repayment-calculator.component";
 import { HowToComponent } from "./how-to/how-to.component";
+import { NgModule } from "@angular/core";
 
-export const appRoutes: Routes = [
-    {path: '', component:RentBuyCalculatorComponent},
-    {path: 'rent-vs-buy', component:RentBuyCalculatorComponent},
-    {path: 'repayment', component:RepaymentCalculatorComponent},
-    {path: 'how-to', component:HowToComponent},
-    {path:'impressum', component:ImpressumComponent},
-    {path:'terms-of-use', component:TermsOfUseComponent},
-]
+const routes: Routes = [
+    { path: '', component: RentBuyCalculatorComponent },
+    { path: 'rent-vs-buy', component: RentBuyCalculatorComponent },
+    { path: 'repayment', component: RepaymentCalculatorComponent },
+    { path: 'how-to', component: HowToComponent },
+    { path: 'impressum', component: ImpressumComponent },
+    { path: 'terms-of-use', component: TermsOfUseComponent },
+  ]
 
+@NgModule({
+    imports: [RouterModule.forRoot(routes, {
+      useHash: false,
+      scrollPositionRestoration: 'enabled',
+    })],
+    exports: [RouterModule],
+  })
+  
 export class AppRoutingModule {
 
 }
